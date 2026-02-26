dad3924bbeafe14e1f44ae526d49ed39030d5d8c
AGonzalez <alejandrajimenezgonzalez13@gmail.com>
2026-02-25 22:47:13 -0500

    Agrego informacion personal en Main

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@
 public class Main {
     public static void main(String[] args) {
 
-
+          System.out.println("Maria Alejandra Gonzalez Jimenez");
+          System.out.println("Mis hobbies son montar biclicleta, escuchar musica,bailar");
+           System.out.println("No practico nada ");
     }
 }
